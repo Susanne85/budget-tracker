6b3e02f4a5038a02930c0d43979164f45a72fc3e
Susanne85 <xkqk2010@gmail.com>
2021-07-17 14:38:43 +0800

    Updated index js

--- public/index.js
+++ public/index.js
@@ -3,7 +3,45 @@
 let transactions = [];
 let myChart;
 //Check if user is online and MONGO database can be updated from cache
+function useIndexedDB(databaseName, storeName, method, object) {
 
+  return new Promise((resolve, reject) => {
+      const request = window.indexedDB.open(databaseName, 1);
+      let db, store;
+
+      request.onupgradeneeded = ({ event }) => {
+          db = request.result;
+          db.createObjectStore(storeName, { keyPath: "_id" });
+      };
+
+      request.onerror = (event) => {
+          console.log('IndexDB had an error', event);
+        };
+      request.onsuccess = (event) => {
+          //Setup a transaction store
+          db = event.target.result;
+          
+          const budgetTransaction = db.transaction([storeName], "readwrite");
+          store = budgetTransaction.objectStore(storeName);
+
+          if (method === "put") {
+             store.put({_id:uuidv4(), budgetData: object});
+          }
+          if (method === "clear") {
+              store.clear(object);
+          }
+          if (method === "get") {
+              const all = store.getAll();
+              all.onsuccess = function () {
+                  resolve(all.result);
+              };
+          }
+          budgetTransaction.oncomplete = function () {
+              db.close();
+          };
+      };
+  });
+}
 if (window.navigator.onLine) {
   //Check the Cache
   useIndexedDB('budget', 'budget', 'get', '').then(results => {
